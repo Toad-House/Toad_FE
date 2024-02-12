2918c8080f0e798bf9bb7501652405b0a45fd8c7
정희수 <jeonghuisu@jeonghuisuui-MacBookAir.local>
2024-02-12 11:34:47 +0900

    :recycle: [Product Page] code refactoring

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,6 @@ function Navigation() {
   const [isMenuVisible, setMenuVisible] = useState(false)
   const [isMobileMenuVisible, setMobileMenuVisible] = useState(false)
 
-  // Step 2: Add event handler function
   const toggleMenu = () => {
     setMenuVisible(!isMenuVisible)
   }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,6 @@ import Main from './Main'
 import Material from './Material'
 import ProductsMain from './products/ProductsMain'
 import ProductDetail from "./products/ProductDetail";
+import ProductUpload from './products/ProductUpload';
 
-export { Main, Material, ProductsMain,ProductDetail }
+export { Main, Material, ProductsMain,ProductDetail,ProductUpload }
