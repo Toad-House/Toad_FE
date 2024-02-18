d03acabcd52f0f2c5103c3146508c625d125bcc8
석찬 윤 <ysc0731@ajou.ac.kr>
2024-02-18 23:30:21 +0900

    🔀 Develop->feat/Material
    merge: Develop->feat/Material

--- src/pages/materials/MaterialMain.jsx
+++ src/pages/materials/MaterialMain.jsx
@@ -1,26 +1,26 @@
 import Navigation from '../../components/Navigation'
 import TabBar from '../../components/TabBar'
 import SearchBar from '../../components/SearchBar'
-import MaterialCard from '../../components/MaterialCard'
-import React, { useState, useEffect } from 'react';
-import { GetAllMaterialsApi } from '../../apis/materials';
+import MaterialCard from './MaterialCard'
+import React, { useState, useEffect } from 'react'
+import { GetAllMaterialsApi } from '../../apis/materials'
 
 export default function MaterialMain() {
-  const [materials, setMaterials] = useState([]);
+  const [materials, setMaterials] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await GetAllMaterialsApi();
-        setMaterials(response);
+        const response = await GetAllMaterialsApi()
+        setMaterials(response)
         console.log(response)
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error('Error fetching products:', error)
       }
-    };
+    }
 
-    fetchData();
-}, []);
+    fetchData()
+  }, [])
 
   return (
     <div>
